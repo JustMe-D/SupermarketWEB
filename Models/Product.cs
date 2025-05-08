545fa931baeb365c20297d34d5e8e9c26abc4a6b
Miguel <miguelangelbernal09@gmail.com>
2025-05-07 20:45:09 -0500

    Create Products

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SupermarketWEB.Models
 {
@@ -11,7 +12,7 @@ namespace SupermarketWEB.Models
         public decimal Price { get; set; }
         public int Stock { get; set; }
         public int CategoryId { get; set; }
-        public Category Category { get; set; }
+        public Category? Category { get; set; }
 
     }
 
--- Pages/Products/Index.cshtml.cs
+++ Pages/Products/Index.cshtml.cs
@@ -20,7 +20,7 @@ namespace SupermarketWEB.Pages.Products
         {
             if (_context.Products != null)
             {
-                Products = await _context.Products.ToListAsync();
+                Products = await _context.Products.Include(p => p.Category).ToListAsync();
             }
         }
     }
