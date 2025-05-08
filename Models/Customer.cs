e90671cc2830753b4c8c270427988048d60d7e0e
Miguel <miguelangelbernal09@gmail.com>
2025-05-08 15:29:47 -0500

    Error Fixes and Index Customers

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -3,12 +3,12 @@
     public class Customer
     { 
         public int Id { get; set; } 
-        public string DocumentNumber { get; set; } 
-        public string FirstName { get; set; } 
-        public string LastName { get; set; } 
-        public string? Address { get; set; }
-        public DateTime? Birthday { get; set; }
-        public string? PhoneNumber { get; set; }
-        public string? Email { get; set; }
+        public string document_number { get; set; } 
+        public string first_name { get; set; } 
+        public string last_name { get; set; } 
+        public string? address { get; set; }
+        public DateTime? birthday { get; set; }
+        public string? phone_number { get; set; }
+        public string? email { get; set; }
     }
 }
